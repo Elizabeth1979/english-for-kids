9d5f8f0f61e6b19022471ed36a58647fd3e2d494
Elizabeth Patrick <el.patrick79@gmail.com>
2023-01-20 17:02:27 +0200

    Adding filtering function

--- script.js
+++ script.js
@@ -1,28 +1,55 @@
 import data from "./data.json" assert { type: "json" };
 
-const heading = document.getElementById("h1");
 const animalList = document.getElementById("animals-list");
-const menuBtn = document.getElementById("menuBtn");
-const menu = document.getElementById("menu");
+const filterDropdown = document.getElementById("filter");
+const dataArr = data;
+
+displayList(dataArr);
+
+filterDropdown.addEventListener('change', filterResults);
+
+function filterResults(e) {
+    const value = this.value;
+    console.log(value);
+    const filteredArr = dataArr.filter((item) => item.category === {value});
+    console.log("filter", filteredArr);
+    // displayList(filteredArr);
+}
+
+// display the list of animals
+function displayList(data) {
+    console.log(data);
+    let listItems = "";
+    for (let i = 0; i < data.length; i++) {
+        let item = data[i];
+        // Why += and not =+
+        listItems += `<li>
+            <h2>${item.name}</h2>
+            <button onclick="document.getElementById(${item.id}).play();">
+                <img alt=${item.alt} src=${item.image}>
+            </button>
+            <audio id=${item.id} src=${item.audio}></audio>
+            <p><span aria-hidden="true">&#128172</span><span class="sr-only">pronounciation</span>${item.pronounciation}</p>
+        </li>`;
+    }
+    // Why not append?
+    animalList.innerHTML += listItems;
+}
+
+// const menuBtn = document.getElementById("menuBtn");
+// const menu = document.getElementById("menu");
 
 // Menu operability - is this the best way?
-menuBtn.addEventListener('click', () => {
-    menu.classList.toggle('close');
-});
+// menuBtn.addEventListener('click', () => {
+//     menu.classList.toggle('close');
+// });
 
 // Audio play - listener on buttons - how to implement
 // target element is image and not button with click
-animalList.addEventListener('click', (e) => {
-    console.log(e);
-    console.log(this);
-})
-
-// get items into arr from json
-console.log(data);
-
-// Filter json file
-const seaAnimalsArr = data.filter((item) => item.category === "fish");
-const insectsArr = data.filter((item) => item.category === "insects");
+// animalList.addEventListener('click', (e) => {
+//     console.log(e);
+//     console.log(this);
+// })
 
 // Check which page we are on
 // let currentArr = [];
@@ -32,23 +59,6 @@ const insectsArr = data.filter((item) => item.category === "insects");
 //     currentArr = insectsArr;
 // }
 
-// display the list of animals
-let listItems = "";
-for (let i = 0; i < data.length; i++) {
-    let item = data[i];
-    // Why += and not =+
-  listItems += `<li>
-    <h2>${item.name}</h2>
-    <button onclick="document.getElementById(${item.id}).play();">
-        <img alt=${item.alt} src=${item.image}>
-    </button>
-    <audio id=${item.id} src=${item.audio}></audio>
-    <p><span aria-hidden="true">&#128172</span><span class="sr-only">pronounciation</span>${item.pronounciation}</p>
-</li>`;
-}
-
-// Why not append?
-animalList.innerHTML += listItems;
 
 
 /*
@@ -60,7 +70,7 @@ animalList.innerHTML += listItems;
 6. Save to google without node modules
 7. At first render display all animals - any performance issue?
 8. More categories: Nature, MAmmals, Birds
-9.  
+9. How do I know if its a string or a number?
 */
 
 /*
